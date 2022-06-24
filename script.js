4a808f7c85b4e0815878a608b89854e2080ab289
Daniel Haas <daniel.c.haas@gmail.com>
2022-06-24 13:39:08 -0400

    Created table and book population. Filled out sparse html for table and new book button. Javascript updated to populate table with book details. Need to add "new book" button functionality and user input.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// DOM elements
+let tbl = document.querySelector('.tbody');
+
+// setup the library array
+let myLibrary = [];
+
 // make a JS object constructor
 function Book(title, author, numPages,read) {
     this.title = title;
@@ -9,3 +15,42 @@ function Book(title, author, numPages,read) {
     }
 }
 
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    
+    myLibrary.forEach((book)=> {
+        // create a table entry for the book
+        let row = document.createElement("tr");
+        let title = document.createElement("td");
+        let author = document.createElement("td");
+        let pages = document.createElement("td");
+        let finished = document.createElement("td");
+        let removeTd = document.createElement("td");
+        let remove = document.createElement("button");
+        remove.innerHTML = "Delete";
+        removeTd.appendChild(remove);
+
+        title.innerHTML = book.title;
+        author.innerHTML = book.author;
+        pages.innerHTML = book.numPages;
+        finished.innerHTML = book.read;
+
+        row.appendChild(title);
+        row.appendChild(author);
+        row.appendChild(pages);
+        row.appendChild(finished);
+        row.appendChild(removeTd);
+        tbl.appendChild(row);
+    });
+}
+
+const b1 = new Book('Pelican Brief', 'Grisham',580,true);
+const b2 = new Book('Behind Enemey Lines', 'Clancy',343,false);
+
+addBookToLibrary(b1);
+addBookToLibrary(b2);
+
+displayBooks();
