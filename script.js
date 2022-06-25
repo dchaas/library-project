07e6a5be07515de725d29d3d302b8f9035382143
Daniel Haas <daniel.c.haas@gmail.com>
2022-06-25 07:00:07 -0400

    Site functionality completed. Added ability to change read status and remove books. Filtering done on library class by book title. Next steps are to refactor JS code and change book list to a card style.

--- script.js
+++ script.js
@@ -26,6 +26,10 @@ function Book(title, author, numPages,read) {
     }
 }
 
+Book.prototype.readBook = function() {
+    this.read = !this.read;
+}
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
@@ -40,37 +44,71 @@ function displayBooks() {
         let title = document.createElement("td");
         let author = document.createElement("td");
         let pages = document.createElement("td");
-        let finished = document.createElement("td");
+        let finishedTd = document.createElement("td");
+        let finished = document.createElement("button");
         let removeTd = document.createElement("td");
         let remove = document.createElement("button");
         remove.innerHTML = "Delete";
+        remove.setAttribute("data",book.title);
+        remove.classList.add('delete-btn');
+        remove.addEventListener('click', (event) => {
+            deleteBook(event.target.getAttribute("data"));
+        });
+        
+        finished.setAttribute("data",book.title);
+        finished.addEventListener('click', (event) => {
+            markRead(event.target.getAttribute("data"));
+        });
+
         removeTd.appendChild(remove);
+        finishedTd.appendChild(finished);
 
         title.innerHTML = book.title;
         author.innerHTML = book.author;
         pages.innerHTML = book.numPages;
-        finished.innerHTML = book.read;
+        finished.innerHTML = book.read ? "Yes" : "No";
+        if (book.read) {
+            finished.classList.add('read-book');
+        }
 
         row.appendChild(title);
         row.appendChild(author);
         row.appendChild(pages);
-        row.appendChild(finished);
+        row.appendChild(finishedTd);
         row.appendChild(removeTd);
         tbl.appendChild(row);
     });
 }
 
-//const b1 = new Book('Pelican Brief', 'Grisham',580,true);
-//const b2 = new Book('Behind Enemey Lines', 'Clancy',343,false);
+const b1 = new Book('Pelican Brief', 'Grisham',580,true);
+const b2 = new Book('Behind Enemey Lines', 'Clancy',343,false);
 
 addBook.addEventListener('click', function() {
     const book = new Book(titleEntry.value,authorEntry.value,pagesEntry.value,readEntry.checked);
-    console.log(book);
+    titleEntry.value = "";
+    authorEntry.value = "";
+    pagesEntry.value = "";
+    readEntry.checked = false;
     addBookToLibrary(book);
     console.log(myLibrary);
     displayBooks();
     formDiv.classList.add('hidden');
 });
 
+
+function deleteBook(bTitle) {
+    myLibrary = myLibrary.filter(book => book.title!==bTitle);
+    displayBooks();
+}
+
+function markRead(bTitle) {
+    let id = myLibrary.findIndex(book => {return book.title===bTitle}); 
+    console.log(myLibrary[id]);
+    myLibrary[id].readBook();
+    displayBooks();
+}
+
+
 addBookToLibrary(b1);
 addBookToLibrary(b2);
+displayBooks();
